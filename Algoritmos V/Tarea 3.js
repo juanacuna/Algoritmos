7be81ea46ceae6104fd300a37a068740f1ed1114
Juan Carlos Acuña <jca710@outlook.com>
2020-02-10 21:57:05 -0300

    Modificacion de Tarea 3

--- Algoritmos V/Tarea 3.js
+++ Algoritmos V/Tarea 3.js
@@ -1,9 +1,9 @@
 function inverso(arr){
   var arr2=[]
-  for(var i=arr.length; i >= 0; i--){
+  for(var i=arr.length-1; i >= 0; i--){
     arr2.push(arr[i]);
   }
   return arr2;
 }
-res= inverso([2,8,4,1,43]);
+res= inverso([2,8,4,1,43,5,7,9,0]);
 console.log(res);
